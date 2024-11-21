afaa818ae0595cd7cbae0a6c5371e5677dae2b1c
Timothy Do <timothydobsa@gmail.com>
2024-11-21 00:33:08 -0800

    Test json, more for debug tomorrow

--- photography/prepareSite.py
+++ photography/prepareSite.py
@@ -1,38 +1,51 @@
 import os
 import cv2 as cv
+import json
+from PIL import Image 
+from PIL.ExifTags import TAGS
 from tqdm import tqdm
 
 fullDir = './fulls'
 thumbDir = './thumbs'
 metadataDir ='./metadata'
+for createDir in [fullDir, thumbDir, metadataDir]: 
+    if(not(os.path.exists(createDir))): os.mkdir(createDir)
 imgNameJS = './imgNames.js'
-imgTS = [imgName.split('.jpg')[0] for imgName in os.listdir(fullDir) if imgName.endswith('.jpg')]
+imgNames = [imgName.split('.jpg')[0] for imgName in os.listdir(fullDir) if imgName.endswith('.jpg')]
 newline = '\n'
 empty = '' 
 
 def makeTSList(split=2):
     with open(imgNameJS,'w') as file:
         arrayStr = 'const imgNames = ['
-        for i,ts in tqdm(enumerate(imgTS)): 
-            arrayStr += f"\'{ts}\'{'' if i == len(imgTS) - 1 else f',{newline if i % split == split-1 else empty}'}"
+        for i,name in tqdm(enumerate(imgNames)): 
+            arrayStr += f"\'{name}\'{'' if i == len(imgNames) - 1 else f',{newline if i % split == split-1 else empty}'}"
         arrayStr += '];'
         file.write(arrayStr)
 
 def getThumbs(check=False):
-    for ts in tqdm(imgTS):
-        thumbPath = f'./{thumbDir}/{ts}.jpg'
-        fullPath = f'{fullDir}/{ts}.jpg'
+    for name in tqdm(imgNames):
+        thumbPath = f'./{thumbDir}/{name}.jpg'
+        fullPath = f'{fullDir}/{name}.jpg'
         if(not(check) or not(os.path.exists(thumbPath))):
             img = cv.imread(fullPath)
             resized = cv.resize(img,(400,300))
             cv.imwrite(thumbPath,resized)
 
 def getMetadatas(check=False):
-    for ts in tqdm(imgTS):
-        fullPath = f'{fullDir}/{ts}.jpg'
-        metadataPath = f'{metadataDir}/{ts}.json'
+    for name in tqdm(imgNames):
+        fullPath = f'{fullDir}/{name}.jpg'
+        metadataPath = f'{metadataDir}/{name}.json'
         if(not(check) or not(os.path.exists(metadataPath))):
-            pass
+            img = Image.open(fullPath)
+            exifdata = img.getexif()
+            metadata = {}
+            for tagid , value in exifdata.items():
+                tagname = TAGS.get(tagid,tagid)
+                metadata[tagname] = value
+            print(metadata)
+            with open(metadataPath,'w') as metadataFile:
+                json.dump(metadata,metadataFile)
 
 if __name__ == '__main__':
     print('Making Timestamp List!');
