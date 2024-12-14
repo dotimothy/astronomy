260105cf4f0e780145e0e1fea8c86f1417915b29
Timothy Do <timothydobsa@gmail.com>
2024-12-14 02:37:18 -0800

    Major Refresh to astro website :) for mobile

--- photography/prepareSite.py
+++ photography/prepareSite.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import cv2 as cv
 import json
 from tqdm import tqdm
@@ -7,8 +8,6 @@ import exifread
 fullDir = './fulls'
 thumbDir = './thumbs'
 metadataDir ='./metadata'
-for createDir in [fullDir, thumbDir, metadataDir]: 
-    if(not(os.path.exists(createDir))): os.mkdir(createDir)
 imgNameJS = './imgNames.js'
 imgNames = [imgName.split('.jpg')[0] for imgName in os.listdir(fullDir) if imgName.endswith('.jpg')]
 imgNames.sort()
@@ -23,14 +22,14 @@ def makeImgList(split=2):
         arrayStr += '];'
         file.write(arrayStr)
 
-def getThumbs(check=False,size=(400,300)):
+def getThumbs(check=False,size=(400,300),quality=80):
     for name in tqdm(imgNames):
         thumbPath = f'./{thumbDir}/{name}.jpg'
         fullPath = f'{fullDir}/{name}.jpg'
         if(not(check) or not(os.path.exists(thumbPath))):
             img = cv.imread(fullPath)
             resized = cv.resize(img,size)
-            cv.imwrite(thumbPath,resized,[cv.IMWRITE_JPEG_QUALITY,80])
+            cv.imwrite(thumbPath,resized,[cv.IMWRITE_JPEG_QUALITY,quality])
 
 def getFileSize(filePath):
     return os.stat(filePath).st_size
@@ -51,10 +50,20 @@ def getMetadatas(check=False):
             with open(metadataPath, 'w') as metadataFile:
                 json.dump(metadata,metadataFile,indent=4)
 
+def cleanState(): 
+    if(os.path.exists(thumbDir)): 
+        shutil.rmtree(thumbDir)
+    if(os.path.exists(metadataDir)): 
+        shutil.rmtree(metadataDir)
+    for createDir in [thumbDir, metadataDir]: 
+        if(not(os.path.exists(createDir))): os.mkdir(createDir)
+
 if __name__ == '__main__':
-    print('Making Image List!');
+    print('Cleaning Previous State...')
+    cleanState()
+    print('Making Image List!')
     makeImgList()
     print('Making Thumbnails!')
-    getThumbs(check=False)
+    getThumbs(check=False,size=(1600,1200),quality=70)
     print('Extracting Metadata!')
     getMetadatas(check=False)
